8958724a3c2fc5a399296913f7df9ccd309f67fa
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-21 16:56:43 +0200

    ADD: solution with regex in the function 03-conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function howMany (string) {
+    let regex = /^[aeiouAEIOU]+$/;
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (regex.test(string.charAt(i))) {
+            count += 1;
+        }
+    }
+    return count;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+// console.log(howMany('areeeaiUr'));
+console.log(howMany(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
