c406a5923755e865ebb36739173b0dfc80954d50
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-21 17:28:10 +0200

    ADD: solution to 05-bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,17 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function sayHiAtTime (who, time = 10) {
+    if (time < 0 || time > 24) {
+        console.error('The time is incorrect');
+        return false;
+    }
+    let message = time < 14 ? `Buongiorno ${who}` : (time < 18 ? `Buon pomeriggio ${who}` : `Buonasera ${who}`);
+    return message;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(sayHiAtTime(name, 18));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
