c74927430c2899e0b1d82582faa998cf0c3c2189
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-21 16:29:51 +0200

    ADD: solutions to script 01-Iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+// function arrayInitialLetter (array) {
+//     let intialArray = []
+//     for ( let i = 0; i < array.length; i++) {
+//         intialArray.push((array.at(i)).charAt(0))
+//     }
+//     return intialArray
+// }
+
+let arrayInitialLetter = (array) => {
+    let intialArray = []
+    for ( let i = 0; i < array.length; i++) {
+        intialArray.push((array.at(i)).charAt(0))
+    }
+    return intialArray
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(arrayInitialLetter(names))
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
